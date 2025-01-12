1ddac01de20f9e0d8388b153b2a40f124a6da7df
ehsanellahi0308 <ehsanvicky0@gmail.com>
2025-01-12 12:51:37 +0500

    Assignment#09

---
+++ script.js
@@ -0,0 +1,24 @@
+/*let n = 10; 
+let i = 1; 
+while (i <= n) {
+    console.log(i);
+    i++; 
+}*/
+
+
+
+/*let n = 10; 
+let i = n; 
+while (i >= 1) {
+    console.log(i);
+    i--; 
+}*/
+
+
+
+let i = 2; 
+while (i <= 100) {
+    console.log(i);
+    i += 2; 
+}
+
